a242731e2b2ef75bed17ba4296c03f1eed3bffb5
OwnBlox <136041487+Kwkwkwjwowj@users.noreply.github.com>
2025-03-16 11:38:50 +0800

    Added /monitor-page endpoint

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const path = require("path");
 
 const app = express();
 
+let totalRequests = 0;
+let clients = [];
+
+app.use((req, res, next) => {
+    totalRequests++;
+    sendUpdateToClients();
+    next();
+});
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -11,8 +20,35 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/monitor-page", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "monitor", "monitor.html"));
+});
+
+app.get("/monitor", (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders();
+    res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+    const clientId = Date.now();
+    const newClient = {
+        id: clientId,
+        res,
+    };
+    clients.push(newClient);
+    req.on("close", () => {
+        clients = clients.filter((client) => client.id !== clientId);
+    });
+});
+
+function sendUpdateToClients() {
+    clients.forEach((client) => {
+        client.res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+    });
+}
+
 const routes = ["igdl", "fbdl", "ttdl", "githubstalk", "searchgroups"];
-routes.forEach(route => {
+routes.forEach((route) => {
     app.use(`/api/${route}`, require(`./api/${route}`));
 });
 
